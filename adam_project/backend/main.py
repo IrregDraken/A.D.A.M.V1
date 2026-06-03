14f4a6c0b8b68c43cc3d448ef56c95b6fe669a84
IrregDraken <allenfriday0@gmail.com>
2026-06-03 21:53:45 +0100

    reworking telegram b2t

--- adam_project/backend/main.py
+++ adam_project/backend/main.py
@@ -1,9 +1,6 @@
 from threading import Thread
-
 from app import app
 from adam_bot import run_bot
-from button_handler import run_button_handler
-from bots_command import run_auto_responder
 
 
 def start_flask():
@@ -14,14 +11,6 @@ def start_telegram_bot():
     run_bot()
 
 
-def start_button_handler():
-    run_button_handler()
-
-
-def start_auto_responder():
-    run_auto_responder()
-
-
 if __name__ == "__main__":
 
     bot_thread = Thread(
@@ -29,18 +18,6 @@ if __name__ == "__main__":
         daemon=True
     )
 
-    button_thread = Thread(
-        target=start_button_handler,
-        daemon=True
-    )
-
-    auto_thread = Thread(
-        target=start_auto_responder,
-        daemon=True
-    )
-
     bot_thread.start()
-    button_thread.start()
-    auto_thread.start()
 
     start_flask()
