009665011293b9c8a8013f85b75df4e6976a4f93
IrregDraken <allenfriday0@gmail.com>
2026-06-05 12:05:57 +0100

    testing command

--- adam_project/backend/app.py
+++ adam_project/backend/app.py
@@ -247,26 +247,25 @@ def fetch_command(device_id):
         "command": None
     })
 
-@app.route("/commands")
-def view_commands():
+@app.route("/debug/commands")
+def debug_commands():
 
     conn = get_connection()
     cursor = conn.cursor()
 
-    cursor.execute("""
+    cursor.execute(
+        """
         SELECT *
         FROM commands
         ORDER BY id DESC
-    """)
+        """
+    )
 
     rows = cursor.fetchall()
 
     conn.close()
 
-    return jsonify([
-        dict(row)
-        for row in rows
-    ])
+    return [dict(row) for row in rows]
 
 if __name__ == "__main__":
     app.run(debug=True)
